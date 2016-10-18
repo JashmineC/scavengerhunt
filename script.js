e07e486dde609e766574db4065c03cac5895d120
ijgreenidge <igreenidge2018@bklawtech.com>
2016-10-18 21:48:53 +0000

    scavenger hunt

---
+++ script.js
@@ -0,0 +1,68 @@
+
+
+console.log("Hello, World!");
+window.alert("What is Your Name");
+window.alert("Hello to you");
+
+var colors= ["Red", "Orange", "Blue", "Black"];
+window.alert(colors[2]);
+
+
+
+  var yay = window.prompt("Do you love ScriptEd?")
+if (yay =="Yes"){
+   window.alert("YAY!");
+}
+ else {
+        window.alert("Boo!");
+    }
+
+var grade = window.prompt("What Grade Are You In?")
+
+if (grade == "9"|| grade== "Nine"){
+    window.alert("You are a Freshman")
+    
+}
+
+else if (grade == "10"|| grade == "Ten"){
+    window.alert("You are a Sophomore");
+    
+}
+else if (grade == "11"|| grade == "Eleven"){
+    window.alert("You are a Junior");
+    
+}
+
+else{
+    window.alert("You are a Senior")
+}
+
+var avg= window.prompt("What is your number grade?")
+
+
+if (avg<60){
+    window.alert("Grade: F")
+    
+ 
+}
+
+else if (avg<70){
+    window.alert("Grade: D")
+}
+else if (avg<80){
+    window.alert("Grade: C")
+}
+else if (avg<70){
+    window.alert("Grade: B")
+}
+else if (avg<100){
+    window.alert("Grade: A")
+}
+
+var roman= window.prompt("Type in a number")
+
+if ()
+
+
+
+
